6d7051045ee63294319c513fa7923e5de74a0631
dshresta2003 <151508850+dshresta2003@users.noreply.github.com>
2024-02-25 13:51:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+//Basics of JavaScript
+alert("Welcome to my page:");
+console.log("Thanks for visiting");
+
+let num1=5;
+let num2=10;
+let num3=14;
+
+//prints the value in variable num3
+console.log(num3);
+//prints as num1
+console.log("num1")
+
+//let us understand some datatypes
+let num4=6;
+let fnumber1=7.2;
+let string1="Shresta";
+let char1="D";
+let bool=true;
+
+console.log(typeof(num4));
+console.log(typeof(fnumber1));
+console.log(typeof(string1));
+console.log(typeof(char1));
+console.log(typeof(bool));
+
